2c53a84c7310cad0619eb93eec3186705bc75e85
plexustechnology <dhruvi.plexustechnology@gmail.com>
2024-10-11 16:37:35 +0530

    changes

--- src/Component/MainBtn.js
+++ src/Component/MainBtn.js
@@ -20,10 +20,10 @@ const MainButton = () => {
         <>
             <div className='rounded-pill p-2 bg-white d-flex align-items-center justify-content-center ms-3 ms-lg-0' style={{ width: "120px" }}>
                 <div className='border-end border-light border-2 cursor playstore' onClick={handlePlaystoreClick} >
-                    <FontAwesomeIcon icon={faGooglePlay} className='pe-3 fs-3 picon' />
+                    <FontAwesomeIcon icon={faGooglePlay} className='pe-3 fs-4 picon' />
                 </div>
                 <div className='cursor appstore' onClick={handleAppstoreClick}>
-                    <FontAwesomeIcon icon={faAppStore} className='ps-3 fs-3 aicon' />
+                    <FontAwesomeIcon icon={faAppStore} className='ps-3 fs-4 aicon' />
                 </div>
 
             </div>
